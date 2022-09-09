e1fd5192d7950ea77b567b34c083c33772d0c7f5
Diana Park <dianapark411@gmail.com>
2022-09-09 18:37:52 -0300

    Avances 1

---
+++ LogTest/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+try
+{
+    //Logger.Info("Info");
+    Logger.Debug("Debug");
+    System.Console.ReadKey();
+}
+catch (Exception ex)
+{
+    Logger.Error(ex, "Goodbye cruel world");
+}
